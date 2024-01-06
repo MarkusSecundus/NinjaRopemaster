9666069f82ab0cd4305e6451a4922d4c876d731f
MarkusSecundus <kubahronik.g@gmail.com>
2024-01-06 22:19:07 +0100

    rope partially working

--- scripts/movement/rope/RopeController.gd
+++ scripts/movement/rope/RopeController.gd
@@ -7,9 +7,16 @@ class_name RopeController
 @onready var anchor_point : StaticBody2D = $AnchorBody;
 @onready var end_point : Node2D = $EndPointMarker;
 
+func _unhandled_key_input(event):
+	if event.is_action_pressed("Jump"):
+		create_the_rope();
 
-func _create_the_rope()->void:
-	var direction = end_point.position - anchor_point.position
+var _already_created := false
+func create_the_rope()->void:
+	if _already_created: return
+	_already_created = true
+	print("starting rope creation!")
+	var direction = end_point.global_position - anchor_point.global_position
 	var length_remaining = direction.length()
 	
 	var last_body : PhysicsBody2D = anchor_point
@@ -19,8 +26,18 @@ func _create_the_rope()->void:
 		self.add_child(new_segment)
 		var joint = new_segment.get_node("Joint") as PinJoint2D
 		var anchor_point_marker = new_segment.get_node("AnchorPointMarker") as Node2D
+		var segment_size = joint.global_position - anchor_point_marker.global_position
+		
+		new_segment.global_position =  last_point.global_position - joint.position
+		print("adding a segment at {0} ({1}) - segment size is {2}".format([new_segment.global_position, new_segment.position, segment_size]))
+		var to_rotate = GeometryUtils.look_at_rotation_rad(segment_size) - GeometryUtils.look_at_rotation_rad(direction) 
+		new_segment.rotation += to_rotate
+		
+		joint.node_b = last_body.get_path()
+		
+		length_remaining -= segment_size.length()
+		last_body = new_segment
+		last_point = anchor_point_marker
 		
 		
-		pass
-	
-	pass
+	print("rope created")
---
+++ scripts/util/GeometryUtils.gd
@@ -0,0 +1,5 @@
+class_name GeometryUtils
+
+
+static func look_at_rotation_rad(direction: Vector2)->float:
+	return atan2(direction.y, direction.x)
