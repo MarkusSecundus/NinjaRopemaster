846709c401ecd3ffa5be7d7664a6d24d1e0cdd5f
MarkusSecundus <kubahronik.g@gmail.com>
2024-01-06 14:23:49 +0100

    add jump press tolerance

--- scripts/Controller.gd
+++ scripts/Controller.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 @export var speed: int = 400
 @export var jump_force: int = 750
 
+@export var jump_press_tolerance_seconds : float = 0.3
 
 # Set the gravity from project settings to be synced with RigidBody nodes
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * 2
@@ -22,8 +23,7 @@ func _physics_process(delta):
 	else:
 		move_stop()
 		
-	if Input.is_action_just_pressed("Jump"):
-		jump()
+	handle_jumping(Input.is_action_just_pressed("Jump"))
 	
 	move_and_slide()
 	ScreenWrap.wrap_x_cbody(self)
@@ -40,11 +40,15 @@ func move_right():
 func move_stop():
 	velocity.x = 0
 	
-func jump():
-	if ! is_grounded():
-		print("Cannot jump because not grounded!")
-		return
-	velocity.y = -jump_force
+var _last_jump_request_end : float = -INF
+func handle_jumping(jump_was_requested : bool)->void:
+	if is_grounded():
+		if jump_was_requested || (_last_jump_request_end > TimeUtils.seconds_elapsed):
+			_last_jump_request_end = -INF
+			velocity.y = -jump_force
+	elif jump_was_requested:
+		_last_jump_request_end = TimeUtils.seconds_elapsed + jump_press_tolerance_seconds
+	
 
 
 @onready var _feet : GroundChecker = $Feet;
